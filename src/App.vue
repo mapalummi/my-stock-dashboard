b4fb90a38cb4e863e21af00c6baa0f600d8f8809
Marco <marco.palummieri@gmail.com>
2025-09-03 10:36:45 +0200

    refactor: remove HelloWorld component and simplify App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  min-height: 100vh;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011f35 100%);
 }
 </style>
