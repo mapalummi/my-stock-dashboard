0a9162ca6c7e0a9695bbe754d500340e5691f132
Marco <marco.palummieri@gmail.com>
2025-09-03 11:45:29 +0200

    feat: add BaseCard component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <BaseCard title="Mein Dashboard">
+    <p>Willkommen zu meinem Stock Dashboard!</p>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from "./components/Card.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    BaseCard,
+  },
 };
 </script>
 
@@ -16,6 +22,8 @@ body {
 #app {
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011f35 100%);
+  box-sizing: border-box;
 }
 </style>
